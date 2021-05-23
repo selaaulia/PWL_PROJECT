269d40eda983953e6e4e328e88bc4193591c288b
divardhia <divardhiass@gmail.com>
2021-05-23 20:29:42 +0700

    conflict

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,29 +1,4 @@
 <html>
-<<<<<<< HEAD
- <head>
- <title>Sistem Informasi PERPUSTAKAAN</title>
- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap
-/4.4.1/css/bootstrap.min.css" integrity="sha384-
-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="an
-onymous">
- <script src="https://code.jquery.com/jquery3.4.1.slim.min.js" integrity="sha384-
-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="an
-onymous"></script>
- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/poppe
-r.min.js" integrity="sha384-
-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="an
-onymous"></script>
- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootst
-rap.min.js" integrity="sha384-
-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="an
-onymous"></script>
- </head>
- <body>
- <div class="container">
- @yield('content')
- </div>
- </body>
-=======
     <head>
         <title>SISTEM INFROMASI PEMINJAMAN BUKU</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
@@ -36,5 +11,4 @@ onymous"></script>
           @yield('content')
         </div> 
     </body>
->>>>>>> cea142214770bfcd379124c9f652fc52445492d3
 </html>
