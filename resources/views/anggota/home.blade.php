40b7ff0ce28304f284e9b6e35c599b74dd9b9842
selaaulia <selaaulia1307@gmail.com>
2021-06-21 12:20:59 +0700

    Home Anggota

--- resources/views/anggota/home.blade.php
+++ resources/views/anggota/home.blade.php
@@ -1,23 +1,16 @@
-@extends('adminlte::page')
+@extends('layouts.admin')
 
 @section('title', 'Home Anggota')
 
 @section('content_header')
     <h1>Home Anggota</h1>
+    <h5>Selamat datang di Perpustakaan {{$user->name}}.</h5>
 @stop
 
 @section('content')
-    <p>Selamat datang di Perpustakaan {{$user->name}}.</p>
-@stop
-
-@section('css-custom')
-    <link href='fullcalendar/main.css' rel='stylesheet' />
-@endsection
-
-@section('content-custom')
     <div class="row">
-        <div class="col-lg-7">
-            <div class="card card-primary">
+        <div class="col-lg-12">
+            <div class="card card-dark">
                 <div class="card-header">
                     <h3 class="card-title">Aturan Peminjaman</h3>
 
@@ -31,7 +24,7 @@
                     </div>
                 </div>
                 <div class="card-body">
-                    <div class="alert alert-info alert-dismissible" style="margin-top: 20px">
+                    <div class="alert alert-light alert-dismissible" style="margin-top: 20px">
                         <h5><i class="icon fas fa-info"></i> Informasi Aturan Peminjaman</h5>
                         <ol>
                             <li>Waktu Peminjaman maksimal 7 hari</li>
@@ -43,49 +36,8 @@
                       </div>
                 </div>
                <div class="col-lg-5">
-            {{-- calendar --}}
-            <div class="card bg-gradient-success">
-                <div class="card-header border-0 ui-sortable-handle" style="cursor: move;">
-
-                    <h3 class="card-title">
-                        <i class="far fa-calendar-alt"></i>
-                        Calendar
-                    </h3>
-                    <!-- tools card -->
-                    <div class="card-tools">
-                        <!-- button with a dropdown -->
-                        <button type="button" class="btn btn-success btn-sm" data-card-widget="collapse">
-                            <i class="fas fa-minus"></i>
-                        </button>
-                        <button type="button" class="btn btn-success btn-sm" data-card-widget="remove">
-                            <i class="fas fa-times"></i>
-                        </button>
-                    </div>
-                    <!-- /. tools -->
-                </div>
-                <!-- /.card-header -->
-                <div class="card-body pt-0">
-                    <!--The calendar -->
-                    <div id="calendar" style="width: 100%">
-
-                    </div>
-                </div>
-                <!-- /.card-body -->
+               </div>
             </div>
         </div>
     </div>
-    @include('sweetalert::alert')
-@stop
-
-@section('js')
-    <script src='fullcalendar/main.js'></script>
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            var calendarEl = document.getElementById('calendar');
-            var calendar = new FullCalendar.Calendar(calendarEl, {
-                initialView: 'dayGridMonth'
-            });
-            calendar.render();
-        });
-    </script>
 @stop
